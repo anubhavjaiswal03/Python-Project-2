6f41a07914e6194a4fb1d6725ef41a7e59c668a8
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2024-05-03 23:10:10 -0400

    Book.py +1 Function

--- Book.py
+++ Book.py
@@ -50,6 +50,18 @@ class Book(Media):
     def get_book_publisher(self):
         return self.__book_publisher
 
+    def get_details(self) -> str:
+        """
+        Helper function that returns a nice block of string that could be used in Recommendations Tab.
+        """
+        return (f"Title: {self.get_title()}\nAuthor: {self.__book_authors}\nAverage Rating: {self.get_rating()}"
+                f"\nISBN: {self.__book_isbn_number}\nISBN13: {self.__book_isbn13_number}"
+                f"\nLanguage Code:{self.__book_language_code}"
+                f"\nPages: {self.__book_page_count}"
+                f"\nRating Count: {self.__book_rating_count}"
+                f"\nPublication Date: {self.__book_publication_date}"
+                f"\nPublisher: {self.__book_publisher}")
+
 
 if __name__ == '__main__':
     test_file = "Input Files/books1000.csv"
